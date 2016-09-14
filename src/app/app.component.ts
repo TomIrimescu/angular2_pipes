8b35c272816262c3d0f477c5e788714e87495c71
TomIrimescu <tom@irimescu.com>
2016-09-14 12:08:15 -0400

    Added two custom pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styles: [` 
+    .pipes{margin: 32px; padding: 32px;}
+    h4{color:blueviolet;}
+    h2{color:blue;}
+    h1{color:firebrick;}
+    span{font-style: italic;}
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  myValue = 'lowercase';
+  myDate = new Date(2016,5,24);
+  values = ['Milk', 'Bread', 'Beans'];
+  asyncValue = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Data arrives here after three seconds!'), 3000);
+  });
+
+  a: number = 0.259;
+  b: number = 1.3495;
+  c: number = 0.259;
+  d: number = 1.3495;
+  pi: number = 3.141;
+  e: number = 2.718281828459045;
+  object: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
+  gender: string = 'other';
+  inviteMap: any = {
+    'male': 'Invite him.',
+    'female': 'Invite her.',
+    'other': 'Invite it.'
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from "./app.component";
+import { DoublePipe } from "./double.pipe";
+import { FilterPipe } from "./filter.pipe";
+
+
+
 
 @NgModule({
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, DoublePipe, FilterPipe ],
   imports: [ BrowserModule ],
   bootstrap: [ AppComponent ]
 })
